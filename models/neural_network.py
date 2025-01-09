bf33f8aca25b63863fa6ce182f001b0919391d30
matteofuligni <79851638+matteofuligni@users.noreply.github.com>
2025-01-09 22:55:07 +0100

    Refactor neural network model and add QTrainer class
    - Added QTrainer class to handle training steps for the neural network model
    - Implemented train_step method to calculate loss and update model parameters
    - Used MSELoss as the loss function and Adam optimizer for training

--- models/neural_network.py
+++ models/neural_network.py
@@ -94,3 +94,36 @@ class Linear_QNet(nn.Module):
         torch.save(self.state_dict(), file_name)
 
         
+class QTrainer:
+    def __init__(self, model, lr, gamma): #initializing 
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr) #optimizer
+        self.criterion = nn.MSELoss() #loss function
+
+    def train_step(self, state, action, reward, next_state, done): #trainer
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+        if len(state.shape) == 1: #if there 1 dimension
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done,)
+        pred = self.model(state) #using the Q=model predict equation above
+
+        target = pred.clone() #using Qnew = r+y(next predicted Q) as mentionned above
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+
+        self.optimizer.zero_grad() #calculating loss function
+        loss = self.criterion(target, pred)
+        loss.backward()
+        self.optimizer.step()
